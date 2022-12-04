1a15ea35587e5f21cb3998cc8b022f05bf765711
GarikVologodskiy <49877717+GarikVologodskiy@users.noreply.github.com>
2022-12-04 13:10:02 +0500

    base preparing

---
+++ src/test/java/com/example/MicroBook/MicroBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
